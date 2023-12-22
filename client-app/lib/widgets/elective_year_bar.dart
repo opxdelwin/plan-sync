cd176fce8525e8ec957895c434c6f9ce01f8cbc6
Delwin Mathew <84124091+opxdelwin@users.noreply.github.com>
2023-12-23 01:25:25 +0530

    feat: year bar for electives

---
+++ client-app/lib/widgets/elective_year_bar.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:plan_sync/controllers/git_service.dart';
+import 'package:plan_sync/util/logger.dart';
+
+class ElectiveYearBar extends StatefulWidget {
+  const ElectiveYearBar({super.key});
+
+  @override
+  State<ElectiveYearBar> createState() => _ElectiveYearBarState();
+}
+
+class _ElectiveYearBarState extends State<ElectiveYearBar> {
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    return Container(
+      decoration: ShapeDecoration(
+        shape: const StadiumBorder(),
+        color: colorScheme.onBackground,
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: SizedBox(
+        width: 128,
+        height: 48,
+        child: DropdownButtonHideUnderline(
+          child: GetBuilder<GitService>(
+            builder: (serviceController) => DropdownButton<String>(
+              isExpanded: true,
+              elevation: 0,
+              enableFeedback: true,
+              style: TextStyle(color: colorScheme.onBackground),
+              icon: Icon(
+                Icons.arrow_drop_down,
+                color: colorScheme.background,
+              ),
+              value: serviceController.selectedElectiveYear?.toString(),
+              dropdownColor: colorScheme.onBackground,
+              hint: Text(
+                "Year",
+                style: TextStyle(
+                  color: colorScheme.background,
+                  fontSize: 16,
+                ),
+              ),
+              menuMaxHeight: 376,
+              items: serviceController.electiveYears
+                  ?.map((year) => buildMenuItem(
+                        year,
+                        colorScheme.background,
+                      ))
+                  .toList(),
+              onChanged: (String? newSelection) {
+                if (newSelection == null) {
+                  return;
+                }
+                Logger.i(newSelection);
+                serviceController.selectedElectiveYear =
+                    int.parse(newSelection);
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+DropdownMenuItem<String> buildMenuItem(String year, Color color) {
+  return DropdownMenuItem(
+    value: year,
+    child: Text(
+      year,
+      style: TextStyle(color: color),
+    ),
+  );
+}
