c79b8449ccfc12d02826b36b5e50b2e49224b002
Delwin Mathew <84124091+opxdelwin@users.noreply.github.com>
2024-07-11 13:08:25 +0530

    update logger and settings screen

--- client-app/lib/util/logger.dart
+++ client-app/lib/util/logger.dart
@@ -5,10 +5,34 @@ import 'package:flutter/foundation.dart';
 ///
 /// Only prints in debug mode, using `kDebugMode`.
 class Logger {
+  static void _printWarning(String text) {
+    // ignore: avoid_print
+    print('\x1B[33m$text\x1B[0m');
+  }
+
+  static void _printError(String text) {
+    // ignore: avoid_print
+    print('\x1B[31m$text\x1B[0m');
+  }
+
   /// Logs info in console, only in debug mode.
   Logger.i(dynamic log) {
     if (kDebugMode) {
       print(log);
+    }
+    return;
+  }
+
+  Logger.w(dynamic log) {
+    if (kDebugMode) {
+      _printWarning(log);
+    }
+    return;
+  }
+
+  Logger.e(dynamic log) {
+    if (kDebugMode) {
+      _printError(log);
     }
     return;
   }
