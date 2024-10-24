2714f9e1b44b77a86aec970ef938d961d3495fa5
Delwin Mathew <84124091+opxdelwin@users.noreply.github.com>
2024-10-24 22:10:19 +0530

    add button to HUDNoticeWidget

--- client-app/lib/backend/models/remote_config/hud_notices_model.dart
+++ client-app/lib/backend/models/remote_config/hud_notices_model.dart
@@ -1,21 +1,29 @@
 import 'package:get/get.dart';
+import 'package:plan_sync/backend/models/remote_config/hud_notices_action_model.dart';
 import 'package:plan_sync/controllers/app_preferences_controller.dart';
 
 class HudNoticeModel {
   final int id;
   final String title;
   final String description;
+  final ActionModel? action;
 
   const HudNoticeModel({
     required this.id,
     required this.title,
     required this.description,
+    this.action,
   });
 
   factory HudNoticeModel.fromMap(Map data) => HudNoticeModel(
         id: data['id'],
         title: data['title'],
         description: data['description'],
+
+        // include this only if data contains action
+        action: data['action'] != null
+            ? ActionModel.fromJson(data['action'])
+            : null,
       );
 
   // helper functions
